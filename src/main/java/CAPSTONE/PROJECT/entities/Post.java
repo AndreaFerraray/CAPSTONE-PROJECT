868c6d3d4a3e7c25fa7149126b16954fbc8a9d45
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-12-05 16:48:50 +0100

    POST

--- src/main/java/CAPSTONE/PROJECT/entities/Post.java
+++ src/main/java/CAPSTONE/PROJECT/entities/Post.java
@@ -1,4 +1,21 @@
 package CAPSTONE.PROJECT.entities;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name="Post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long postId;
+    private String foto;
+    private String content;
+    private String luogo;
+
 }
